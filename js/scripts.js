d305cb531a6339b937284ec4d3d0653cdfed05b0
Joyce Querubino <joycequerubino5@gmail.com>
2020-09-16 12:26:13 -0300

    Adicionando transição

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-// Menu lateral
+// Menu lateral com transição para as páginas
 const nav = document.querySelector(".itens-menu"), 
     navList = nav.querySelectorAll("li"), 
     totalNavList =  navList.length, 
@@ -10,19 +10,32 @@ for(let i=0; i < totalNavList; i++){
     const a = navList[i].querySelector("a"); 
     console.log(a)
     a.addEventListener("click", function(){
+        
+        //Removendo a classe back-ection
+        for (let i=0; i < totalSection; i++){
+            allSection[i].classList.remove("back-section");
+        }
+
         for(let j=0; j<totalNavList; j++){
+
+            if(navList[j].querySelector("a").classList.contains("active")){
+                // Adicionando a classe back-section
+                allSection[j].classList.add("back-section");
+            }
             navList[j].querySelector("a").classList.remove("active"); 
+        
         }
         this.classList.add("active");
-
         showSection(this);
     })
 }
 
 function showSection(element){
+
     for (let i=0; i < totalSection; i++){
         allSection[i].classList.remove("active");
     }
+
     const target = element.getAttribute("href").split("#")[1];
           console.log(target)  
           document.querySelector("#" + target).classList.add("active")
